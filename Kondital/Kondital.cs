ab6d9200aa39aa3979f92797f6a16ac185c86e07
Simon Andreassen <simo531i@elevcampus.dk>
2021-01-21 11:59:38 +0100

    Added health condition dictionaries.

--- Kondital/Kondital.cs
+++ Kondital/Kondital.cs
@@ -6,6 +6,23 @@ namespace Kondital
 {
     class Kondital
     {
+        Dictionary<Tuple<int, int>, List<Tuple<int, int>>> femaleHealthCondition = new Dictionary<Tuple<int, int>, List<Tuple<int, int>>>()
+        {
+            { new Tuple<int, int>(20, 29), new List<Tuple<int, int>>() { new Tuple<int, int>(Int32.MinValue, 28), new Tuple<int, int>(29, 34), new Tuple<int, int>(35, 43), new Tuple<int, int>(44, 48), new Tuple<int, int>(49, Int32.MaxValue) } },
+            { new Tuple<int, int>(30, 39), new List<Tuple<int, int>>() { new Tuple<int, int>(Int32.MinValue, 27), new Tuple<int, int>(28, 33), new Tuple<int, int>(34, 41), new Tuple<int, int>(42, 47), new Tuple<int, int>(48, Int32.MaxValue) } },
+            { new Tuple<int, int>(40, 49), new List<Tuple<int, int>>() { new Tuple<int, int>(Int32.MinValue, 25), new Tuple<int, int>(26, 31), new Tuple<int, int>(32, 40), new Tuple<int, int>(41, 45), new Tuple<int, int>(46, Int32.MaxValue) } },
+            { new Tuple<int, int>(50, 65), new List<Tuple<int, int>>() { new Tuple<int, int>(Int32.MinValue, 21), new Tuple<int, int>(22, 28), new Tuple<int, int>(29, 36), new Tuple<int, int>(37, 41), new Tuple<int, int>(42, Int32.MaxValue) } }
+        };
+        
+        Dictionary<Tuple<int, int>, List<Tuple<int, int>>> maleHealthCondition = new Dictionary<Tuple<int, int>, List<Tuple<int, int>>>()
+        {
+            { new Tuple<int, int>(20, 29), new List<Tuple<int, int>>() { new Tuple<int, int>(Int32.MinValue, 38), new Tuple<int, int>(39, 43), new Tuple<int, int>(44, 51), new Tuple<int, int>(52, 56), new Tuple<int, int>(57, Int32.MaxValue) } },
+            { new Tuple<int, int>(30, 39), new List<Tuple<int, int>>() { new Tuple<int, int>(Int32.MinValue, 34), new Tuple<int, int>(35, 39), new Tuple<int, int>(40, 47), new Tuple<int, int>(48, 51), new Tuple<int, int>(52, Int32.MaxValue) } },
+            { new Tuple<int, int>(40, 49), new List<Tuple<int, int>>() { new Tuple<int, int>(Int32.MinValue, 30), new Tuple<int, int>(31, 35), new Tuple<int, int>(36, 43), new Tuple<int, int>(44, 47), new Tuple<int, int>(48, Int32.MaxValue) } },
+            { new Tuple<int, int>(50, 59), new List<Tuple<int, int>>() { new Tuple<int, int>(Int32.MinValue, 25), new Tuple<int, int>(26, 31), new Tuple<int, int>(32, 39), new Tuple<int, int>(40, 43), new Tuple<int, int>(44, Int32.MaxValue) } },
+            { new Tuple<int, int>(60, 69), new List<Tuple<int, int>>() { new Tuple<int, int>(Int32.MinValue, 21), new Tuple<int, int>(22, 26), new Tuple<int, int>(27, 35), new Tuple<int, int>(36, 39), new Tuple<int, int>(40, Int32.MaxValue) } }
+        };
+
         /// <summary>
         /// Calculate "kondital" based on heart rate.
         /// </summary>
